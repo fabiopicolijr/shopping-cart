2328d0ae56daaeca4391d8830eaa7c3ba4ecfd17
Fábio Picoli <fabiopicolijr@gmail.com>
2022-01-12 19:46:26 -0300

    fixing a mistake

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -51,7 +51,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     if (cartPreviousValue !== cart) {
       localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
     }
-  });
+  }, [cart, cartPreviousValue]);
 
   const addProduct = async (productId: number) => {
     try {
